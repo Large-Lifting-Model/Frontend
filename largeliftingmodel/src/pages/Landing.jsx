2d3d320c5d7e2f7e98dbb85d7275cfef216b7e64
Ryan Baker <ryanwbaker87@gmail.com>
2024-10-02 18:43:36 +0100

    Updated Landing Page

--- largeliftingmodel/src/pages/Landing.jsx
+++ largeliftingmodel/src/pages/Landing.jsx
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom";
 import PageNav from "../components/PageNav";
 import styles from "./Landing.module.css";
 
@@ -5,7 +6,15 @@ function Landing() {
 	return (
 		<main className={styles.landing}>
 			<PageNav />
-			<div>Landing</div>
+			<section>
+				<h1>
+					<br />
+					On Demand Customized Workouts
+				</h1>
+				<Link to="/Register" className="cta">
+					Start Tracking Now
+				</Link>
+			</section>
 		</main>
 	);
 }
