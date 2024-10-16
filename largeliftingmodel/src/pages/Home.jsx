e6d38ac887758ef6e196b798f3a5e1662f006776
Riley-Martel <79805828+Riley-Martel@users.noreply.github.com>
2024-10-16 01:43:13 -0600

    Created Initial Home Page
    Initial draft of home page created. Need too complete the styling for the welcome message, and the workout suggestion. Edited the cta style in index .css to add hover functionality as well as predefined widths. Also added some new colours

--- largeliftingmodel/src/pages/Home.jsx
+++ largeliftingmodel/src/pages/Home.jsx
@@ -1,11 +1,62 @@
 import AppNav from "../components/AppNav";
+import styles from "./Home.module.css";
+import { Link } from "react-router-dom";
+import { useState, useEffect,} from 'react'
+//import {useNavigate} from "react-router-dom"
+
+
+const dummyUsername = "<username>"
+
+const dummySuggestion = "<Suggested workout from LLM>"
+
+
+
+
+
+//Will need api call for the suggested workout
 
 function Home() {
+
+	const [username, setUsername] = useState(dummyUsername);
+	const [suggestion, setSuggestion] = useState(dummySuggestion);
+
+	//const navigate = useNavigate();
+
+	useEffect(() => {
+		setUsername(dummyUsername); // replace with API data
+		setSuggestion(dummySuggestion); // replace with API data
+    }, []);
+
 	return (
-		<div>
+		<main className={styles.home}>
 			<AppNav />
-			<div>Home</div>
-		</div>
+			<section>
+			<h1>
+				Welcome, {username}
+			</h1>
+
+			<p className={styles.suggestion}>
+				{suggestion}
+			</p>
+
+			<div className={styles.links}>
+
+				<Link to="/create" className="cta">
+					Create Workout
+				</Link>
+				<Link to="/history" className="cta">
+					Past Workouts
+				</Link>
+				<Link to="/profile" className="cta">
+					Profile
+				</Link>
+
+			</div>
+			</section>
+
+
+
+		</main>
 	);
 }
 
