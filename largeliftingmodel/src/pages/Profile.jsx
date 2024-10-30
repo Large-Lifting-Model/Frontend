8f7579b8f0ca5941d960914ba153e4e7be07ee81
twilson1111 <98843866+twilson1111@users.noreply.github.com>
2024-10-30 14:26:36 -0600

    CRUD implemented for profile using test server

--- largeliftingmodel/src/pages/Profile.jsx
+++ largeliftingmodel/src/pages/Profile.jsx
@@ -9,13 +9,37 @@ import buttonStyles from "../components/Button.module.css"
 import formStyles from "../components/Form.module.css"
 import {useNavigate} from "react-router-dom"
 
-const dummyLoginInfo = {
+const testUserID = "12903781273"
+
+const testProfile = {
+	id: testUserID.toString(),
+	firstName: "testUser_fName",
+	lastName: "testUser_lname",
+	email: "testUser_e@mail"
+};
+
+const emptyProfile = {
+	id: "",
 	firstName: "",
 	lastName: "",
 	email: ""
 };
 
-const dummyHealthInfo = {
+const testHealth = {
+	id: testUserID.toString(),
+	dob: "November 20 2004",
+	gender: "Male",
+	height: 172,
+	weight: 90,
+	favoriteWorkoutType: "weights",
+	workoutExperience: "medium",
+	fitnessGoal: "hypertrophy",
+	injuries: "bad left knee",
+	otherConsiderations: "none"
+}
+
+const emptyHealth = {
+	id: "",
 	dob: "",
 	gender: "",
 	height: 0,
@@ -32,35 +56,167 @@ const dummyHealthInfo = {
 
 function Profile() {
 	
-	const [loginInfo, setLoginInfo] = useState(dummyLoginInfo);
-	const [wipLoginInfo, setWIPLoginInfo] = useState(dummyLoginInfo);
-	const [healthInfo, setHealthInfo] = useState(dummyHealthInfo);
-	const [wipHealthInfo, setWIPHealthInfo] = useState(dummyHealthInfo);
+	const [loginInfo, setLoginInfo] = useState(emptyProfile);
+	const [wipLoginInfo, setWIPLoginInfo] = useState(emptyProfile);
+	const [healthInfo, setHealthInfo] = useState(emptyHealth);
+	const [wipHealthInfo, setWIPHealthInfo] = useState(emptyHealth);
 	const [isEditingLoginInfo, setIsEditingLoginInfo] = useState(false);
 	const [isEditingHealthInfo, setIsEditingHealthInfo] = useState(false);
 
+	const [error, setError] = useState(null);
+	const [loading, setLoading] = useState(true)
+
 	const navigate = useNavigate();
 
+	const getOrCreateProfile = async () => {
+		try {
+			const profileResponse = await fetch('http://localhost:8000/profile/' + testUserID.toString());
+			if (!profileResponse.ok) throw new Error('Profile fetch failed');
+			const profile = await profileResponse.json();
+			setLoginInfo(profile);
+			setWIPLoginInfo(profile);
+		} catch (error) { // If it has been deleted, re-create it.
+			console.info("CREATING test profile")
+			const testCreateResponse = await fetch('http://localhost:8000/profile/', {
+				method: 'POST', // Specify PUT method
+				headers: {
+				  'Content-Type': 'application/json', // Indicate JSON content
+				},
+				body: JSON.stringify(testProfile)
+			})
+			if (!testCreateResponse.ok) throw new Error('Creating new test profile failed');
+			setLoginInfo(testProfile);
+			setWIPLoginInfo(testProfile)
+		}
+	}
+
+	const getOrCreateHealth = async () => {
+		try {
+			const response = await fetch('http://localhost:8000/health/' + testUserID.toString());
+			if (!response.ok) throw new Error('Health fetch failed');
+			const health = await response.json();
+			setHealthInfo(health);
+			setWIPHealthInfo(health);
+		} catch (error) { // If it has been deleted, re-create it.
+			console.info("CREATING test health")
+			const testCreateResponse = await fetch('http://localhost:8000/health/', {
+				method: 'POST', // Specify PUT method
+				headers: {
+				  'Content-Type': 'application/json', // Indicate JSON content
+				},
+				body: JSON.stringify(testHealth)
+			})
+			if (!testCreateResponse.ok) throw new Error('Creating new test health failed');
+			setHealthInfo(testHealth);
+			setWIPHealthInfo(testHealth);
+		}
+	}
+
+	const getAllData = async () => {
+		try {
+			setLoading(true);
+			await getOrCreateProfile()
+			await getOrCreateHealth()
+
+		} catch (err) {
+			console.error("GetOrCREATE - " + err.message)
+			setError(err.message);
+		} finally {
+			setLoading(false); // Ensure loading is stopped after fetch completes
+		}
+		
+	}
+
+	const putProfile = async (profileData) => {
+		try {
+			setLoading(true);
+			const profileResponse = await fetch('http://localhost:8000/profile/' + testUserID.toString(), {
+				method: 'PUT', // Specify PUT method
+				headers: {
+				  'Content-Type': 'application/json', // Indicate JSON content
+				},
+				body: JSON.stringify(profileData)
+			})
+			if (!profileResponse.ok) throw new Error('Profile PUT failed');
+
+		} catch (err) {
+			console.error("I HAVE AN ERROR FOR SOME REASON")
+			console.info(err)
+			setError(err.message);
+		} finally {
+			setLoading(false); // Ensure loading is stopped after fetch completes
+			setLoginInfo(profileData)
+			setWIPLoginInfo(profileData)
+			setIsEditingLoginInfo(false)
+		}
+	}
+
+	const putHealth = async (healthData) => {
+		try {
+			setLoading(true);
+			const healthResponse = await fetch('http://localhost:8000/health/' + testUserID.toString(), {
+				method: 'PUT', // Specify PUT method
+				headers: {
+				  'Content-Type': 'application/json', // Indicate JSON content
+				},
+				body: JSON.stringify(healthData)
+			})
+			if (!healthResponse.ok) throw new Error('Health PUT failed');
+
+		} catch (err) {
+			console.error(err)
+			setError(err.message);
+		} finally {
+			setLoading(false); // Ensure loading is stopped after fetch completes
+			setHealthInfo(healthData)
+			setWIPHealthInfo(healthData)
+			setIsEditingHealthInfo(false)
+		}
+	}
+
+	const delProfile = async () => {
+		try {
+			setLoading(true);
+			const profileResponse = await fetch('http://localhost:8000/profile/' + testUserID.toString(), {
+				method: 'DELETE', // Specify PUT method
+			})
+			if (!profileResponse.ok) throw new Error('Profile DELETE failed');
+			const healthResponse = await fetch('http://localhost:8000/health/' + testUserID.toString(), {
+				method: 'DELETE', // Specify PUT method
+			})
+			if (!healthResponse.ok) throw new Error('Health DELETE failed');
+		} catch (err) {
+			console.error(err)
+			setError(err.message);
+		} finally {
+			setLoading(false); // Ensure loading is stopped after fetch completes
+			setLoginInfo(emptyProfile);
+			setWIPLoginInfo(emptyProfile);
+			setHealthInfo(emptyHealth);
+			setWIPHealthInfo(emptyHealth);
+			setIsEditingLoginInfo(false);
+			setIsEditingHealthInfo(false);
+			navigate('..');
+		}
+	}
+
+	
+
 	useEffect(() => {
-		setLoginInfo(dummyLoginInfo); // replace with API data
-		setWIPLoginInfo(dummyLoginInfo); // replace with API data
-		setHealthInfo(dummyHealthInfo); // replace with API data
-		setWIPHealthInfo(dummyHealthInfo); // replace with API data
+		getAllData();
     }, []);
+
+
 
 	const handleSubmit = (event) => {
 		event.preventDefault();
 		const formName = event.target.name;
 		switch(formName) {
 		case "loginInfo":
-			alert(`Saving loginInfo:  "${JSON.stringify(wipLoginInfo)}". This is a placeholder for API call`);
-			setLoginInfo(wipLoginInfo)
-			setIsEditingLoginInfo(false)
+			putProfile(wipLoginInfo)
 			break;
 		case "healthInfo":
-			alert(`Saving healthInfo:  "${JSON.stringify(wipHealthInfo)}". This is a placeholder for API call`);
-			setHealthInfo(wipHealthInfo)
-			setIsEditingHealthInfo(false)
+			putHealth(wipHealthInfo)
 			break;
 		default:
 
@@ -68,14 +224,7 @@ function Profile() {
 	};
 
 	const deleteProfile = () => {
-		alert(`Deleting Profile. This is a placeholder for API call`);
-		setLoginInfo(dummyLoginInfo);
-		setWIPLoginInfo(loginInfo);
-		setHealthInfo(dummyHealthInfo);
-		setWIPHealthInfo(healthInfo);
-		setIsEditingLoginInfo(false);
-		setIsEditingHealthInfo(false);
-		navigate('..');
+		delProfile()
 	}
 
 	const cancelEditingLoginInfo = () => {
